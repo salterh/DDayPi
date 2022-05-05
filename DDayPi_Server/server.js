1d4acf7bad8e733eaf78727d51210ecc9e81b66f
Harry Salter <harry.salter@port.ac.uk>
2022-05-05 15:42:21 +0100

    Working on the radio puzzle

--- DDayPi_Server/server.js
+++ DDayPi_Server/server.js
@@ -1,28 +1,14 @@
 const express = require("express");
-const { redirect } = require("express/lib/response");
 const app = express();
 
 app.listen(3000, "localhost");
 app.use(express.static("public"));
 
-app.get('/puzzleone/:complete', puzzleOne);
-app.get('/puzzleone/:reset', puzzleOne);
+app.get("/:one/:complete", handleRequest);
 
-function puzzleOne(req, res) {
-    var message;
-    switch(Object.keys(req.params)[0]) {
-        case "complete":
-            message = {
-                address: "/puzzlecomplete",
-                info: "add"
-            }
-        break;
-        case "reset":
-            message = {
-                address: "/reset",
-                info: "reset"
-            }
-        break;
-    }
-    res.send(message);
+function handleRequest(req, res) {
+    res.send({
+        puzzle: Object.keys(req.params)[0],
+        status: Object.keys(req.params)[1]
+    });
 }
