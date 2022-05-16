b73048f05b121eb1173d8d40319ead4e16dce483
Harry Salter <harry.salter@port.ac.uk>
2022-05-16 09:49:09 +0100

    Update server.js

--- DDayPi_TeachableMachine/server.js
+++ DDayPi_TeachableMachine/server.js
@@ -1,16 +1,4 @@
 const express = require("express");
 const app = express();
-
 app.listen(3000, "localhost");
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 app.use(express.static("public"));
-
-app.get("/:puzzleone", puzzleOne);
-
-function puzzleOne(req, res) {
-    res.send("hello");
-}
