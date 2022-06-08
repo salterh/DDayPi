318fb03fb0e0a178972061a16873fa256777d9f1
Harry Salter <harry.salter@port.ac.uk>
2022-06-08 10:44:26 +0100

    Added rpio

--- Handheld/server.js
+++ Handheld/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const rpio = require("rpio");
 const app = express();
 app.listen(3000, "localhost");
 app.use(express.static("public"));
+
+app.get("/gpio", (req, res) => {
+    res.send({
+        data: "justsomedata"
+    });
+})
