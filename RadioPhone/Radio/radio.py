4f090d15bd298ad1c250992839492a453915353b
Harry Salter <harry.salter@port.ac.uk>
2022-07-05 09:23:27 +0000

    Handheld python

--- RadioPhone/Radio/radio.py
+++ RadioPhone/Radio/radio.py
@@ -1,18 +1,40 @@
 import gpiozero as gpio
-from pydub import AudioSegment
-from pydub.playback import play
+import pygame
+import numpy
 import random
 
-freq1 = gpio.RotaryEncoder(15, 18)
-freq2 = gpio.RotaryEncoder(23, 24)
-vol = gpio.RotaryEncoder(7, 1)
+def preload():
+    global noise
+    global sample
+    pygame.mixer.init(frequency=44100)
+    noiseList = numpy.random.uniform(-1,1,44100)
+    noise = pygame.mixer.Sound(noiseList)
+#    sample = pygame.mixer.Sound("./Audio/sample.wav")
+    setup()
+
+def setup():
+    pygame.mixer.Sound.play(noise, loops=-1)
+    print(noise.get_raw())
+
+preload()
+
+while True:
+    global vol
+    vol = scale(volPot.value,-1,1,0,1)
+    noise.set_volume(vol)
+    
+
+#freq1Pot = gpio.RotaryEncoder(15, 18)
+#freq2Pot = gpio.RotaryEncoder(23, 24)
+#volPot = gpio.RotaryEncoder(7, 1)
 freqRange = 150
 
 def scale(oldValue, oldMin, oldMax, newMin, newMax):
     return (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin)
 
 def setCentre():
-    global center = random.range(0, 1000)
+    global center
+    center = random.range(0 + freqRange, 1000 - freqRange)
+     
 
-
-            
+    
