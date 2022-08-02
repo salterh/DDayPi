c708d6bc86de285595727ac42d3600c597e9fb4b
Harry Salter <harry.salter@port.ac.uk>
2022-08-02 11:34:04 +0000

    Radio - potentiometers working

--- RadioPhone/Radio/radio.py
+++ RadioPhone/Radio/radio.py
@@ -2,8 +2,8 @@ import gpiozero as gpio
 import pygame
 import numpy
 import random
-<<<<<<< HEAD
 import asyncio
+import time
 
 freqRange = 150
 freq1Pot = gpio.MCP3008(channel=0)
@@ -16,95 +16,33 @@ def scale(oldValue, oldMin, oldMax, newMin, newMax):
 
 def setMiddle():
     global middle
-    middle = random.range(0, 1000)
-=======
-import time
-
-freqRange = 150
-freq1 = 0, freq2 = 1000
-middle = 500, range = 150
-counter = 0
-
-freq1Pot = gpio.MCP3008(0)
-freq2Pot = gpio.MCP3008(1)
-volPot = gpio.MCP3008(2)
->>>>>>> 3295df7ff5dbbaa66126025a5ff322ac2a448dd3
+    middle = random.randrange(0, 1000)
+    print(middle)
 
 def preload():
+    setMiddle()
     global noise
     global sample
     pygame.mixer.init(frequency=44100)
     noiseList = numpy.random.uniform(-1,1,44100)
     noise = pygame.mixer.Sound(noiseList)
-<<<<<<< HEAD
 #    sample = pygame.mixer.Sound("./Audio/sample.wav")
     pygame.mixer.Sound.play(noise, loops=-1)
 #    pygame.mixer.Sound.play(sample, loops=-1)
 
-def timerFunc():
-    counter+=1
-    if counter > 30:
-        setCentre()
-
 preload()
 
 while True:
-    combi = (freq1Pot.value / freq2Pot.value) / 2
-    if abs(combi - middle) > range:
-        if (combi >= middle): scaled = scale(combi, middle, middle + freqRange, 1, 0)
-        else: scaled = scale(combie, middle, middle - freqRange, 1, 0)
+    freq1 = scale(freq1Pot.value, 0, 1, 0, 1000)
+    freq2 = scale(freq2Pot.value, 0, 1, 0, 1000)
+    combi = (freq1+freq2) / 2
+    if abs(combi - middle) < freqRange:
+        if combi >= middle:
+            scaled = scale(combi, middle, middle + freqRange, 1, 0)
+        else: scaled = scale(combi, middle, middle - freqRange, 1, 0)
     else: scaled = 0
+    print(scaled)
 #    sample.set_volume(scaled * volPot.value)
-    noise.set_volume(scale(scaled, 0, 1, 1, 0) * volPot.value)
-        
-    
-
-
-     
-=======
-    sample = pygame.mixer.Sound("./Audio/sample.wav")
-    setup()
-
-def setup():
-    pygame.mixer.Sound.play(noise, loops=-1)
-    countUp()
-    
-    
-def countUp():
-    counter += 1
-    if counter > 30:
-        setMiddle()
-        counter = 0
-    print(counter)
-    time.sleep(1)
-    countUp()
-    
-def reset():
-    counter = 0
-
-def scale(oldValue, oldMin, oldMax, newMin, newMax):
-    return (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin)
-
-def setCentre():
-    global middle
-    middle = random.range(0 + freqRange, 1000 - freqRange)
->>>>>>> 3295df7ff5dbbaa66126025a5ff322ac2a448dd3
-
-def print_val():
-    print(testPot.steps)
-    print(testPot.value)
-    
-testPot = gpio.RotaryEncoder(20,21, wrap = False, max_steps = 100)
-testPot.when_rotated = print_val
-    
-while True:
-    freq1 = freq1Pot.value
-    freq2 = freq2Pot.value
-    combi = (freq1 + freq2) / 2
-    scaled = 0
-    if abs(combi - middle) < range:
-        if combi >= centre: scaled = scale(combi, middle, middle + range, 1, 0)
-        else: scaled = scale(combi, middle, middle - range, 1, 0)
-    else scaled = 0
-    sample.set_volume(scaled * volPot.value)
-    noise.set_volume(scale(scaled, 0, 1, 1, 0) * volPot.value)
+    noiseVol = scale(scaled, 0,1,1,0) * volPot.value
+    noise.set_volume(noiseVol)
+    time.sleep(0.2)
