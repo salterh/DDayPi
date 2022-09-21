caa2ef691121c0eb719c78efa1aefc574628a4d9
Harry Salter <harry.salter@port.ac.uk>
2022-09-21 13:08:19 +0000

    PD patch works with phone. TODO with all other projects

--- Phone/phoneInput.py
+++ Phone/phoneInput.py
@@ -1,30 +1,30 @@
 import gpiozero as gp
-import subprocess
+import subprocess as sp
 
-subprocess.call("puredata -nogui -audioaddoutdev \"snd_rpi_hifiberry_dac\" playSound.pd", shell=True)
-
-dialCount = gp.Button(23)                                                                            
-dialStart = gp.Button(18)
+dialCount = gp.Button(25)                                                                            
+dialStart = gp.Button(24)
 count = -1
 answer = [1,1,1]
 playerInput = []
+##sp.call("puredata -nogui -audioaddoutdev \"snd_rpi_hifiberry_dac\" playSound.pd", shell=True)
+
 
 def addCount():
     global count
     count+=1
     
 def dialRelease():
-    global data
     global count
     global playerInput
     if count >= 10:
         count = 0
     playerInput.append(count)
+    print(playerInput)
     count = -1
     if len(playerInput) == len(answer):
         if playerInput == answer:
             print("Done")
-            subprocess.call("puredata -nogui -audioaddoutdev \"snd_rpi_hifiberry_dac\" playSound.pd", shell=True)
+            sp.call("puredata -nogui -audioaddoutdev \"snd_rpi_hifiberry_dac\" playSound.pd", shell=True)
         else:
             print("Try again")
         playerInput = []
