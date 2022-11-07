353f2587a7b44942805158a7b394de841a6b1b78
Harry Salter <harry.salter@port.ac.uk>
2022-11-07 11:59:34 +0000

    renaming for clarity

--- Phone/net_phone.py
+++ Phone/net_phone.py
@@ -4,6 +4,7 @@ from oscpy.client import OSCClient as oscC
 import schedule
 
 osc = oscC("127.0.0.1", 8000)
+print("Starting")
 
 dialCount = gp.Button(25)                                                                            
 dialStart = gp.Button(24)
@@ -12,16 +13,20 @@ count = -1
 def addCount():
     global count
     count+=1
+    print(count)
     
 def dialRelease():
     global count
-    print(count)
+    print("release: ", count)
     osc.send_message(b"/play", [count])
     count = -1
     
-
+def dialPressed():
+    print("Working")
+    
 dialCount.when_released = addCount
 dialStart.when_released = dialRelease
+dialStart.when_pressed = dialPressed
 
 while True:
     pass
