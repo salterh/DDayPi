f15bc15900d8b7f4ca9837181cea1a44568b08fd
Harry Salter <harry.salter@port.ac.uk>
2022-11-21 17:04:56 +0000

    Radio puzzle changed - no interaction

--- Radio/radio.sh
+++ Radio/radio.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo Loading Python radio.py
-python3 radio.py &
 echo loading Puredata radio.pd 
-puredata -nogui -audioaddoutdev "snd_rpi_hifiberry_dac" radio.pd
+puredata -audioaddoutdev "snd_rpi_hifiberry_dac" radio.pd
